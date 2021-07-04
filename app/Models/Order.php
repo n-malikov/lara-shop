64d812aba76454049a7b34bed356303c31e87b87
malikov <malikov.kzn@gmail.com>
2021-07-05 00:11:23 +0300

    lesson 10 - Auth redirect and orders

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -13,6 +13,11 @@ class Order extends Model
         return $this->belongsToMany(Product::class)->withPivot('count')->withTimestamps();
     }
 
+//    public function user()
+//    {
+//        return $this->belongsTo(User::class);
+//    }
+
     public function getFullPrice() // подсчет суммы всего, что есть в корзине
     {
         $sum = 0;
